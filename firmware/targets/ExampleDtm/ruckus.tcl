b8c929b6c80657431fe4b80fa91c952dbae68dc0
Benjamin Reese <bareese@slac.stanford.edu>
2022-03-11 11:21:26 -0800

    Add simulation

--- firmware/targets/ExampleDtm/ruckus.tcl
+++ firmware/targets/ExampleDtm/ruckus.tcl
@@ -11,5 +11,9 @@ loadRuckusTcl $::env(TOP_DIR)/submodules/surf
 loadRuckusTcl $::env(TOP_DIR)/submodules/rce-gen3-fw-lib/DtmCore
 
 # Load target's source code and constraints
-loadSource      -dir  "$::DIR_PATH/hdl/"
+loadSource      -lib ldmx -dir "$::DIR_PATH/hdl/"
 loadConstraints -dir  "$::DIR_PATH/hdl/"
+
+loadSource       -lib ldmx -sim_only -dir "$::DIR_PATH/sim/"
+
+set_property top {ExampleDtmTb} [get_filesets {sim_1}]
