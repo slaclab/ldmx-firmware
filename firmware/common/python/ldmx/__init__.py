8aebcec1a20124202be7301f498669ccf089599f
Benjamin Reese <bareese@slac.stanford.edu>
2022-05-20 14:20:39 -0700

    Remove unused modules and add new ones

--- firmware/common/python/ldmx/__init__.py
+++ firmware/common/python/ldmx/__init__.py
@@ -1,45 +1,28 @@
-from hps._Ad5144 import *
-from hps._Ad9252 import *
-from hps._Ads1115 import *
-from hps._Ads7924 import *
-from hps._Tca6424a import *
-from hps._Apv25 import *
-from hps._ControlData import *
-from hps._ControlDpm import *
-from hps._DataDpm import *
-from hps._DataPath import *
-from hps._DataReadout import *
-from hps._EventBuilder import *
-from hps._FebBootloaderFpga import *
-from hps._FebConfig import *
-from hps._FebCore import *
-from hps._FebFpga import *
-from hps._FebFpgaOld import *
-from hps._FebSem import *
-from hps._Hybrid import *
-from hps._HybridSyncStatus import *
-from hps._Ltc2991 import *
-from hps._RceCore import *
-from hps._ApvDataFormatter import *
-from hps._SoftPowerMonitor import *
-from hps._StreamLogger import *
-from hps._PcieTiDtm import *
-from hps._TiRegisters import *
-from hps._HpsSvtDaq import *
-from hps._RceTcpServer import *
-from hps._DaqMap import *
-from hps._HpsDataTools import *
-from hps._epics import *
-from hps._JLabTimingPcie import *
-from hps._CodaSvtDaqRoot import *
-from hps._CodaDummyRoot import *
-from hps._ControlDpmRoot import *
-#from hps._FebStatus import *
-
-CODA_RUN_STATES = [
-    'Download',
-    'Prestart',
-    'Go',
-    'Pause',
-    'End',
-    'Stopped']
+from ldmx._Ad5144 import *
+from ldmx._Ad9252 import *
+from ldmx._Ads1115 import *
+from ldmx._Ads7924 import *
+from ldmx._Tca6424a import *
+from ldmx._Apv25 import *
+from ldmx._EventBuilder import *
+from ldmx._FebConfig import *
+from ldmx._FebCore import *
+from ldmx._HpsFebHw import *
+from ldmx._HpsFebPgp import *
+from ldmx._HpsFeb import *
+from ldmx._Hybrid import *
+#from ldmx._HybridSyncStatus import *
+from ldmx._Ltc2991 import *
+from ldmx._ApvDataFormatter import *
+from ldmx._ApvDataPipeline import *
+from ldmx._SoftPowerMonitor import *
+#from ldmx._StreamLogger import *
+#from ldmx._FebStatus import *
+from ldmx._DaqTiming import *
+from ldmx._HpsFebPGoodMon import *
+from ldmx._HpsFebRoot import *
+from ldmx._HybridPowerControl import *
+from ldmx._HybridSyncStatus import *
+from ldmx._PhaseShifter import *
+from ldmx._HybridDataCore import *
+from ldmx._IntermediatePowerControl import *
