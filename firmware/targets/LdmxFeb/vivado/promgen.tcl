aa2a147499b6727d286dca499b5e67c8cd417857
Benjamin Reese <bareese@slac.stanford.edu>
2023-10-06 12:36:03 -0700

    Set prom for SPIx4

--- firmware/targets/LdmxFeb/vivado/promgen.tcl
+++ firmware/targets/LdmxFeb/vivado/promgen.tcl
@@ -4,8 +4,8 @@
 # Refer to UG835 write_cfgmem for options
 
 set format     "mcs"
-set inteface   "spix1"
-set size       "256"
+set inteface   "spix4"
+set size       "512"
 # set loadbit    "up 0x02000000 ${inputFile}"
 set loadbit    "up 0x00400000 ${inputFile}"
 
