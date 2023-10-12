80ea2cd7e6f1c0644bc393851c3b020a595f7317
Christos Bakalis <cbakalis@slac.stanford.edu>
2023-10-12 03:09:46 -0700

    opt align checking script

--- software/scripts/PgpLaneAlignChecker.py
+++ software/scripts/PgpLaneAlignChecker.py
@@ -16,7 +16,9 @@ import ldmx
 
 rogue.Logging.setFilter('pyrogue.SrpV3', rogue.Logging.Debug)
 
+##########################################################################
 # inherit everything from the original parser and add a couple more stuff
+##########################################################################
 class PgpLaneAlignCheckerParser(ldmx.TrackerPciePgpFcArgParser):
     def __init__(self, **kwargs):
         super().__init__(**kwargs)
@@ -28,7 +30,9 @@ class PgpLaneAlignCheckerParser(ldmx.TrackerPciePgpFcArgParser):
             type     = int,
             required = False,
             default  = 2,
-            help     = "How many seconds to sleep between polling intervals.",
+            help     = "How many seconds to sleep between polling intervals. Important to set this \
+                        value to at least 8 if resetting the retry counter in-between multiple \
+                        readouts. Value is irrelevant if oneRead flag is set to True.",
         )
 
         self.add_argument(
@@ -36,27 +40,85 @@ class PgpLaneAlignCheckerParser(ldmx.TrackerPciePgpFcArgParser):
             action = 'store_true',
             default = False)
 
+        self.add_argument(
+            "--rstRetryCnt",
+            action = 'store_true',
+            default = False)
+
+
+#########################
+# Bittware handling class
+#########################
+class BittwareHandler():
+    def __init__(self, board, args):
+        # arguments
+        self.board         = board
+        self.args          = args
+
+        # variables
+        self.lanes         = []
+        self.quads         = []
+        self.lockFlags     = []
+        self.rxClkFreqs    = []
+        self.retryCnts     = []
+        self._formatDigits = 0
+        self._format       = None
+
+    def cntReset(self):
+        if self.args.rstRetryCnt:
+            print("Resetting...please make sure you wait long enough in-between readouts.")
+            for quad in range(args.numLanes):
+                for lane in range(4):
+                    self.board.PgpFc.PgpLane[quad][lane].PgpFcGtyCoreWrapper.GtRxAlignCheck.RstRetryCnt.set(1)
+
+    def statusParser(self):
+        for quad in range(args.numLanes):
+            for lane in range(4):
+                self.quads.append(quad)
+                self.lanes.append(lane)
+
+                self.lockFlags.append(self.board.PgpFc.PgpLane[quad][lane].PgpFcGtyCoreWrapper.GtRxAlignCheck.Locked.get())
+
+                self.rxClkFreqs.append(round((self.board.PgpFc.PgpLane[quad][lane].PgpFcGtyCoreWrapper.GtRxAlignCheck.RxClkFreqRaw.get())*1.0e-6, 5))
+
+                self.retryCnts.append(self.board.PgpFc.PgpLane[quad][lane].PgpFcGtyCoreWrapper.GtRxAlignCheck.RetryCnt.get())
+
+    def _formatter(self):
+        self._formatDigits = len(str(max(self.retryCnts + self.rxClkFreqs)))
+        self._format = 'Quad = {0:<%d}, Lane = {1:<%d}, RxClkFreq = {2:<%d} MHz, RetryCnt = {3:<%d}, Locked = {4:<%d}' % (1, 1, self._formatDigits, self._formatDigits, 1)
+
+    def statusPrinter(self):
+        self._formatter()
+        system('clear')
+        for i in range(len(self.lockFlags)):
+            print(self._format.format(self.quads[i], self.lanes[i], '{0:.5f}'.format(self.rxClkFreqs[i]), self.retryCnts[i], self.lockFlags[i]))
+
+    def _clearAll(self):
+        self.lanes.clear()
+        self.quads.clear()
+        self.lockFlags.clear()
+        self.rxClkFreqs.clear()
+        self.retryCnts.clear()
+
+    def _doAll(self):
+        self.statusParser()
+        self.statusPrinter()
+        self.cntReset()
+        self._clearAll()
+
+
 parser = PgpLaneAlignCheckerParser()
 args = parser.parse_args()
 
-def statusPrinter(root, numLanes):
-    system('clear')
-    for quad in range(numLanes):
-        for lane in range(4):
-            locked       = root.PgpFc.PgpLane[quad][lane].PgpFcGtyCoreWrapper.GtRxAlignCheck.Locked.get()
-            rxClkFreqRaw = root.PgpFc.PgpLane[quad][lane].PgpFcGtyCoreWrapper.GtRxAlignCheck.RxClkFreqRaw.get()
-            # formatting into a string retains the trailing zeros thus resulting in a justified printout
-            rxClkFreq    = '{:.5f}'.format(round(rxClkFreqRaw * 1.0e-6, 5))
-
-            print(f"Quad = {quad} Lane = {lane}, RxClkFreq = {rxClkFreq} MHz, Locked = {locked}")
-
 with ldmx.TrackerPciePgpFcRoot(dev=args.dev, sim=args.sim, numLanes=args.numLanes) as root:
+    handler = BittwareHandler(board=root, args=args)
+
     if args.oneRead is True:
-        statusPrinter(root=root, numLanes=args.numLanes)
+        handler._doAll()
     else:
         while True:
             try:
-                statusPrinter(root=root, numLanes=args.numLanes)
+                handler._doAll()
                 time.sleep(args.sleepTime)
             except KeyboardInterrupt:
                 print("")
