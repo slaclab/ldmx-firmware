951b42f9f92f7302b5b22fe8949decfa678ac101
Christos Bakalis <cbakalis@slac.stanford.edu>
2024-03-11 11:38:15 -0700

    vc number bugfix

--- firmware/common/rtl/LdmxPgpFcLane.vhd
+++ firmware/common/rtl/LdmxPgpFcLane.vhd
@@ -279,7 +279,7 @@ begin
 
    end generate GEN_VC;
 
-   GEN_VC_CHECK : if NUM_VC_EN_G < 4 generate
+   GEN_VC_CHECK : if NUM_VC_EN_G < 3 generate
       -- gnd inputs
       pgpTxMastersGt(3 downto NUM_VC_EN_G) <= (others => AXI_STREAM_MASTER_INIT_C);
       pgpRxCtrlGt(3 downto NUM_VC_EN_G)    <= (others => AXI_STREAM_CTRL_INIT_C);
