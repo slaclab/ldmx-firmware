35a6308f0821e70591131b3f14ba9fa467b9fb90
Benjamin Reese <bareese@slac.stanford.edu>
2024-03-14 00:55:49 -0700

    Add python Device classes corresponding to new firmware modules

---
+++ firmware/common/python/ldmx/_TrackerPgpFcArray.py
@@ -0,0 +1,16 @@
+
+import pyrogue as pr
+import ldmx
+
+class TrackerPgpFcArray(pr.Device):
+    def __init__(self, numPgpQuads, numVc, **kwargs):
+        super().__init__(**kwargs)
+
+        for quad in range(numPgpQuads):
+            # number of lanes is always 4
+            # *not* the same as DMA lanes
+            for lane in range(4):
+                self.add(ldmx.LdmxPgpFcLane(
+                    name = f'PgpLane[{quad*4+lane}]',
+                    numVc = numVc,
+                    offset = 0x1_0000 * (quad*4+lane)
