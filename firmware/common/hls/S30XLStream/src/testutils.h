d422f484e22fa50f4735fbc1ac1c3091a18cc35e
rodwyer100 <rodwyer@stanford.edu>
2024-04-26 10:11:03 -0700

    Update testutils.h

--- firmware/common/hls/S30XLStream/src/testutils.h
+++ firmware/common/hls/S30XLStream/src/testutils.h
@@ -1,6 +1,6 @@
 #ifndef TESTUTILS_H
 #define TESTUTILS_H
-#include "C:\Users\Rory\AppData\Roaming\Xilinx\Vitis\objdef.h"
+#include "objdef.h"
 
 bool compareHit(Hit Hit1, Hit Hit2){
 	return ((Hit1.mID==Hit2.mID)and(Hit1.bID==Hit2.bID)and(Hit1.Amp==Hit2.Amp)and(Hit1.Time==Hit2.Time));//and(Hit1.TrigTime==Hit2.TrigTime));
