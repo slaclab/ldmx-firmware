abd21be26b6ed501cb3156d301f4984112306015
Benjamin Reese <bareese@slac.stanford.edu>
2024-05-16 13:30:25 -0700

    Add ruckus support

---
+++ firmware/common/hls/S30XLStream/ruckus.tcl
@@ -0,0 +1,9 @@
+# Load RUCKUS library
+source -quiet $::env(RUCKUS_DIR)/vivado_proc.tcl
+
+# Load Source Code
+#loadSource    -path "$::DIR_PATH/rtl/AxiStreamExampleWrapper.vhd"
+loadZipIpCore -path "$::DIR_PATH/ip/S30XLStream-v1.0-20240515151057-bareese-dirty.zip" -repo_path $::env(IP_REPO)
+if { [get_ips AxiStreamExample_0] eq ""  } {
+   create_ip -name S30XLStream -vendor SLAC -library hls -version 1.0 -module_name hitproducerStream_hw_0
+}
