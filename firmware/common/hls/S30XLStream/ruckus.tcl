fe4cb9e5dc778803e539f484fb7ec491b0339afd
Benjamin Reese <bareese@slac.stanford.edu>
2024-05-17 14:32:29 -0700

    Use HDL outputs

--- firmware/common/hls/S30XLStream/ruckus.tcl
+++ firmware/common/hls/S30XLStream/ruckus.tcl
@@ -3,7 +3,8 @@ source -quiet $::env(RUCKUS_DIR)/vivado_proc.tcl
 
 # Load Source Code
 #loadSource    -path "$::DIR_PATH/rtl/AxiStreamExampleWrapper.vhd"
-loadZipIpCore -path "$::DIR_PATH/ip/S30XLStream-v1.0-20240515151057-bareese-dirty.zip" -repo_path $::env(IP_REPO)
-if { [get_ips AxiStreamExample_0] eq ""  } {
-   create_ip -name S30XLStream -vendor SLAC -library hls -version 1.0 -module_name hitproducerStream_hw_0
-}
+# loadZipIpCore -path "$::DIR_PATH/ip/S30XLStream-v1.0-20240516132713-bareese-dirty.zip" -repo_path $::env(IP_REPO)
+# if { [get_ips AxiStreamExample_0] eq ""  } {
+#    create_ip -name S30XLStream -vendor SLAC -library hls -version 1.0 -module_name hitproducerStream_hw_0
+# }
+loadSource -lib hls -path "$::DIR_PATH/ip/verilog"
