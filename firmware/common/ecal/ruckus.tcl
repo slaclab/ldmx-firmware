ecdc4f0a03fc956618bc3fadfd08a350d04d0768
Benjamin Reese <bareese@slac.stanford.edu>
2024-05-23 13:30:53 -0700

    Update vhdl lib names for each subsystem

--- firmware/common/ecal/ruckus.tcl
+++ firmware/common/ecal/ruckus.tcl
@@ -2,16 +2,5 @@
 source -quiet $::env(RUCKUS_DIR)/vivado_proc.tcl
 
 # Load ruckus files
-loadSource -lib ldmx -dir "$::DIR_PATH/rtl/" 
-loadSource -lib ldmx -sim_only -dir  "$::DIR_PATH/sim/"
-
-loadIpCore -path "$::DIR_PATH/ip/TsGtyIpCore/TsGtyIpCore.xci" 
-
-
-# loadSource -dir "$::DIR_PATH/verilog/"
-# loadSource -dir "$::DIR_PATH/verilog/pflink"
-# loadSource -dir "$::DIR_PATH/verilog/pflink/core"
-# loadSource -dir "$::DIR_PATH/verilog/pflink/core/pfclk"
-# loadSource -dir "$::DIR_PATH/verilog/pflink/core/pflink"
-# loadSource -dir "$::DIR_PATH/verilog/pflink/utility"
-# loadSource -dir "$::DIR_PATH/verilog/daq"
+loadSource -lib ldmx_ecal -dir "$::DIR_PATH/rtl/" 
+loadSource -lib ldmx_ecal -sim_only -dir  "$::DIR_PATH/sim/"
--- firmware/common/tracker/ruckus.tcl
+++ firmware/common/tracker/ruckus.tcl
@@ -2,16 +2,6 @@
 source -quiet $::env(RUCKUS_DIR)/vivado_proc.tcl
 
 # Load ruckus files
-loadSource -lib ldmx -dir "$::DIR_PATH/rtl/" 
-loadSource -lib ldmx -sim_only -dir  "$::DIR_PATH/sim/"
+loadSource -lib ldmx_tracker -dir "$::DIR_PATH/rtl/" 
+loadSource -lib ldmx_tracker -sim_only -dir  "$::DIR_PATH/sim/"
 
-loadIpCore -path "$::DIR_PATH/ip/TsGtyIpCore/TsGtyIpCore.xci" 
-
-
-# loadSource -dir "$::DIR_PATH/verilog/"
-# loadSource -dir "$::DIR_PATH/verilog/pflink"
-# loadSource -dir "$::DIR_PATH/verilog/pflink/core"
-# loadSource -dir "$::DIR_PATH/verilog/pflink/core/pfclk"
-# loadSource -dir "$::DIR_PATH/verilog/pflink/core/pflink"
-# loadSource -dir "$::DIR_PATH/verilog/pflink/utility"
-# loadSource -dir "$::DIR_PATH/verilog/daq"
