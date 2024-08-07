1f4e8f928fd5346da543a335da3cac5188b6357a
Benjamin Reese <bareese@slac.stanford.edu>
2024-08-07 09:34:15 -0700

    Fix address

--- firmware/common/tdaq/python/ldmx_tdaq/_FcHub.py
+++ firmware/common/tdaq/python/ldmx_tdaq/_FcHub.py
@@ -1,22 +1,18 @@
-import rogue
 import pyrogue as pr
-import surf.xilinx
-import surf.protocols.pgp
 import ldmx_tdaq
 
 class FcHub(pr.Device):
     def __init__(self, numQuads=1, **kwargs):
         super().__init__(**kwargs)
 
+
+        self.add(ldmx_tdaq.FcTxLogic(
+            name   = "FcTxLogic",
+            offset = 0x010_0000))
+
         self.add(ldmx_tdaq.Lcls2TimingRx(
             name   = 'Lcls2TimingRx',
             offset = 0x000000))
-
-        # FcTxLogic has no AXI registers
-        # self.add(ldmx_tdaq.FcTxLogic(
-        #     name   = "FcTxLogic",
-        #     numVc  = 0,
-        #     offset = 0x010000))
 
         for i in range(numQuads*4):
             self.add(ldmx_tdaq.LdmxPgpFcLane(
