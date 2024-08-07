ff34936bc4e8eaf5ee24c7812c10871d265a6cfb
Benjamin Reese <bareese@slac.stanford.edu>
2024-08-07 09:34:45 -0700

    Add TxLogic import

--- firmware/common/tdaq/python/ldmx_tdaq/__init__.py
+++ firmware/common/tdaq/python/ldmx_tdaq/__init__.py
@@ -1,6 +1,7 @@
 from ._FcEmu import *
 from ._FcHub import *
 from ._FcRxLogic import *
+from ._FcTxLogic import *
 from ._LdmxPgpFcLane import *
 from ._FcReceiver import *
 from ._Pgp2fcGtyCoreWrapper import *
